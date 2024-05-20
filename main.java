2516835d30310e8f13914c869901e251b2318c67
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-05-20 15:54:28 +0530

    variables

---
+++ main.java
@@ -0,0 +1,26 @@
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World1");
+//     }
+// }
+
+// class AnotherClass {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World2");
+//     }
+// }
+
+// ------------------------------------------------------------------------------------------------
+
+class Main {
+    static int age2 = 25;
+    
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("hello world3 is " + age2 + " years old");
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+
+
